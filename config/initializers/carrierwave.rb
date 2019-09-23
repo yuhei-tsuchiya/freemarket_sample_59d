ad843491ae865b5fafea5462e6520e35879b6175
kdr250 <ytkodera4863@gmail.com>
2019-09-23 20:16:25 +0900

    初回デプロイ用-S3設定に伴いテストコントローラー作成その2
    初回デプロイ用-S3設定に伴いテストコントローラー作成その2、haml&cssに一旦統一しました。

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -9,7 +9,7 @@ CarrierWave.configure do |config|
     provider: 'AWS',
     aws_access_key_id: Rails.application.credentials.aws[:access_key_id],
     aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],
-    region: 'ap-northeast-1' #例 'ap-northeast-1'
+    region: 'ap-northeast-1'
   }
 
   config.fog_directory  = 'https://s3-ap-northeast-1.amazonaws.com/uploadcarriewavetest'
