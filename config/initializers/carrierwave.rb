3e4abffbf5d8a0fa43cfde8b8159d5fe17ed9516
kdr250 <ytkodera4863@gmail.com>
2019-09-24 11:03:25 +0900

    初回デプロイ用-carrierwave設定修正
    初回デプロイ用-carrierwave設定修正

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,15 +3,20 @@ require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
-  config.storage = :fog
-  config.fog_provider = 'fog/aws'
-  config.fog_credentials = {
-    provider: 'AWS',
-    aws_access_key_id: Rails.application.credentials.aws[:access_key_id],
-    aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],
-    region: 'ap-northeast-1'
-  }
+  if Rails.env.production?
+    config.storage = :fog
+    config.fog_provider = 'fog/aws'
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: Rails.application.credentials.aws[:access_key_id],
+      aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],
+      region: 'ap-northeast-1'
+    }
 
-  config.fog_directory  = 'https://s3-ap-northeast-1.amazonaws.com/uploadcarriewavetest'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/uploadcarriewavetest'
+    config.fog_directory  = 'uploadcarriewavetest'
+    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/uploadcarriewavetest'
+  else
+    config.storage :file
+  end
+
 end
