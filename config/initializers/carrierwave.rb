c1b633865a77027cff30869db3da4390e1f0ddc4
kdr250 <ytkodera4863@gmail.com>
2019-09-24 11:40:43 +0900

    初回デプロイ用-コメント追記
    初回デプロイ用-コメント追記

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,6 +3,7 @@ require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
+  # 本番環境用
   if Rails.env.production?
     config.storage = :fog
     config.fog_provider = 'fog/aws'
@@ -16,6 +17,7 @@ CarrierWave.configure do |config|
     config.fog_directory  = 'uploadcarriewavetest'
     config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/uploadcarriewavetest'
   else
+    # 開発環境用
     config.storage :file
   end
 
