5571ae267cbbadef5ef7429f7c0318212bd1eb41
y-keysynth <ykhojojp@gmail.com>
2019-09-25 17:48:27 +0900

    会員情報入力

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :signup do
+    collection do
+      get 'step1'
+      get 'step2'
+      get 'step3'
+      get 'step4' # ここで、入力の全てが終了する
+      get 'done' # 登録完了後のページ
+    end
+  end
+
 end
