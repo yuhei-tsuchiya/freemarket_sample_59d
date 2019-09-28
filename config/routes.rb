af123b296244158534d2434882bc6b1d99767599
yuhei-tsuchiya <yuhei.1711.555@gmail.com>
2019-09-28 20:12:52 +0900

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  root to: 'toppage#index'
+  # root to: 'toppage#index'
+  root to: 'items#deteal'
   devise_for :users
   get 'testimages' => 'testimages#index'    # S3テスト用ファイルアップロード画面
   post 'testimages' => 'testimages#create'    # S3テスト用ファイルアップロードのPOST
