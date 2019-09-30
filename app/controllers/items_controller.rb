e3e8aeb9407d34aa7bbccba4204229cdbe89b777
kdr250 <ytkodera4863@gmail.com>
2019-09-30 12:46:37 +0900

    DB設定-商品テーブルのアソシエーション設定ver2
    item.rbに、取引状況、配送日数、商品状態のコードを文字列に変換するメソッドを作成しました。

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,6 +23,8 @@ class ItemsController < ApplicationController
   # => #<Size id: 10, name: "M", created_at: "2019-09-30 01:11:26", updated_at: "2019-09-30 01:11:26", ancestry: "1">
   # > item.burden
   # => #<Burden id: 4, name: "ゆうメール", created_at: "2019-09-30 01:11:29", updated_at: "2019-09-30 01:11:29", ancestry: "1">
+  # > item.prefecture
+  # => #<Prefecture:0x00007fb4da47dfb0 @attributes={:id=>3, :name=>"岩手県"}>
 
   # カテゴリーにサイズがあるかどうかの判別方法
   # > cat = Category.find(8)  # コスメ・香水・美容（サイズはない）
@@ -31,5 +33,13 @@ class ItemsController < ApplicationController
   # > cat2 = Category.find(15)  # レディース>トップス（サイズあり）
   # > cat2.size
   # => #<Size id: 1, name: "服サイズ", created_at: "2019-09-30 01:11:26", updated_at: "2019-09-30 01:11:26", ancestry: nil>
+
+  # 取引状況、配送日数、商品状態を文字列に変換するメソッド(item.rbで定義)
+  # > item.display_torihiki_info
+  # => "売却済"
+  # > item.display_shipping_days
+  # => "4~7日"
+  # > item.display_product_state
+  # => "未使用"
 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -7,5 +7,21 @@ class Item < ApplicationRecord
   belongs_to :burden
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
-  # belongs_to :prefecture
+  
+  # torihiki_info、shipping_days、product_stateを文字列に変換するメソッドを定義
+  def display_torihiki_info
+    torihiki_info_list = ["出品中", "取引中", "売却済"]
+    return torihiki_info_list[self.torihiki_info]
+  end
+
+  def display_product_state
+    product_state_list = ["新品", "未使用", "傷なし", "やや傷あり", "やや傷汚れ", "傷汚れ", "状態悪し"]
+    return product_state_list[self.product_state]
+  end
+
+  def display_shipping_days
+    shipping_days_list = ["1~2日", "2~3日", "4~7日"]
+    return shipping_days_list[self.shipping_days]
+  end
+
 end
