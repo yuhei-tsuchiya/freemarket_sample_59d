14dbc55f449b83ab2ab4ee0926e240153f8a88d8
yuhei-tsuchiya <yuhei.1711.555@gmail.com>
2019-10-04 12:57:13 +0900

    サイドメニュー

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # root to: 'toppage#index'
-  root to: 'items#index'
+  root to: 'users#mypage'
   devise_for :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
