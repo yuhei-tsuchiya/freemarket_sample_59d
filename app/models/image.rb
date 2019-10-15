ee4b2147cc27669e59f0e52bfb5ec3428317448a
yuhei-tsuchiya <yuhei.1711.555@gmail.com>
2019-10-15 17:01:11 +0900

    カテゴリーごとに商品表示

--- app/models/image.rb
+++ app/models/image.rb
@@ -3,8 +3,8 @@ class Image < ApplicationRecord
   belongs_to :item,dependent: :destroy
   mount_uploader :image, ImageUploader
 
-  # validates :image, presence: true
-  # validates :item, presence: true
+  validates :image, presence: true
+  validates :item, presence: true
 
 
 
