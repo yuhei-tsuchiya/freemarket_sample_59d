bbe42dce49a174b6f5e26376b763aa6e84b939fd
kdr250 <ytkodera4863@gmail.com>
2019-10-16 16:55:08 +0900

    検索のビューとransack-ver2
    検索のビューとransack-ver2

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,6 +1,5 @@
 #サーバ上でのアプリケーションコードが設置されているディレクトリを変数に入れておく
-# app_path = File.expand_path('../../../', __FILE__)
-app_path = '/var/www/freemarket_sample_59d'
+app_path = File.expand_path('../../../', __FILE__)
 
 #アプリケーションサーバの性能を決定する
 worker_processes 1
